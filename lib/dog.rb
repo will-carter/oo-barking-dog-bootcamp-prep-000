9e080fcabeb6fa96dd2c398bc6072712d49b0ae1
Will Carter <willmckenna64@gmail.com>
2018-08-07 20:25:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog
+  def initialize(name)
+    @name = name
+    bark = puts "woof!"
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+end
